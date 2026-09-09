8d1f03c1131197e6de84cb0d44ca5bd4d2a339a5
Alejandro <Aleboytron11@gmail.com>
2026-09-09 14:53:06 -0400

    Implement ExerciseLibrary screen with basic layout

--- app/src/main/java/com/example/gymquest/ExerciseLibrary.kt
+++ app/src/main/java/com/example/gymquest/ExerciseLibrary.kt
@@ -1,2 +1,25 @@
 package com.example.gymquest
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ExerciseLibrary() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Exercise Library",
+            color = Color.White
+        )
+    }
+}
